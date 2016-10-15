8ecd3ae0d23944bd92b254b40d7ab47257c13433
Tablesalt <samchott@gmail.com>
2016-10-15 18:53:43 -0500

    added basic user auth, chat room functionality

--- learnr/learnrapp/urls.py
+++ learnr/learnrapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'chat/', views.chat, name='chat'),
-    url(r'send_message/', views.send_message, name='send_message')
+    url(r'send_message/', views.send_message, name='send_message'),
+    url(r'register/', views.register, name='register'),
+    url(r'login/', views.login, name='login'),
+    url(r'get_messages/', views.get_messages, name='get_messages')
 ]
