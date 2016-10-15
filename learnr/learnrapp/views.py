3381ebd04b0ac3cf3c152fed4bda58bf4b50cbca
Thomas Littlejohn <tommy-gun101@hotmail.com>
2016-10-15 15:17:56 -0700

    Screen layout coming along. Waiting for Seyeon's new header bar.

--- learnr/learnrapp/views.py
+++ learnr/learnrapp/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("TIME TO CHAT IT UP.")
+	return render(request, 'main_page.html')
+	#return HttpResponse("TIME TO CHAT IT UP.")
+
 
 def chat(request):
 	return render(request, 'chat.html')
+
 
 def send_message(request):
 	return render(request, 'chat.html')
